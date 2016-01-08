1fdf1115186beb944a7d3ba289cd4041aca349c3
Matt <matthewy@1kiwi.com>
2016-01-08 07:49:26 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+end
