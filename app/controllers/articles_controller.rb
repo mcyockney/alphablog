8895f8e5a38db63087abff945bf05ceedf808f9e
Matt <matthewy@1kiwi.com>
2016-01-22 00:51:24 +0000

    Created new teplate action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+    
+    def show
+    end
+    
+    def new
+        @article = Article.new
+        
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to articles_show_path(@article)
+    end
+    
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
+  
+  resources 'articles'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
