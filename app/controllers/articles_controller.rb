6ffac70af495f64e016f17046fe64ecf8f8d825d
Matt <matthewy@1kiwi.com>
2016-02-21 09:22:07 +0000

    Added CRUD for articles, updated the About and home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,20 @@ class ArticlesController < ApplicationController
         @article=Article.find(params[:id]) 
     end
     
+    def destroy
+        @article=Article.find(params[:id]) 
+        @article.destroy
+        flash[:notice] = "Article was removed"
+        redirect_to articles_path
+        
+    end
+    
     def new
         @article = Article.new
+    end
+    
+    def edit
+        @article=Article.find(params[:id]) 
     end
     
     def index
@@ -22,6 +34,15 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end 
     
     private
     def article_params
