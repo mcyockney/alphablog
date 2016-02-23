c31ada97f4bb2e697d7c29fdccfe41fce46c839e
Matt <matthewy@1kiwi.com>
2016-02-23 08:47:14 +0000

    Refactored the set_article code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,9 @@
 class ArticlesController < ApplicationController
-    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]    
     def show
-        @article=Article.find(params[:id]) 
     end
     
     def destroy
-        @article=Article.find(params[:id]) 
         @article.destroy
         flash[:notice] = "Article was removed"
         redirect_to articles_path
@@ -17,7 +15,6 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article=Article.find(params[:id]) 
     end
     
     def index
@@ -35,7 +32,6 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -49,5 +45,8 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title, :description)
     end
     
+    def set_article
+        @article = Article.find(params[:id])
+    end
     
 end
